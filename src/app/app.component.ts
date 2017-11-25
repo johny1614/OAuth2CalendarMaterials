77f4936b854d3f7c8ae720d53a379560fb509485
Michał Lis <johny1614@gmail.com>
2017-11-25 22:43:27 +0100

    dodany sideBar i header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,87 @@
-import { Component } from '@angular/core';
+import {HttpModule,Http,Headers} from '@angular/http';
+import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+export class AppComponent implements OnInit{
+  constructor(public http:Http){}
+  ngOnInit(): void {
+    console.log('w init');
+    this.accessToken=this.getAccessToken(window.location.href)
+    if(window.location.hash!=undefined && window.location.hash!=''){
+    this.accessToken=this.getAccessToken(window.location.hash)
+    }
 
+  }
+  title = 'app';
+  displayedCols = ['summary','description'];
+  calCols=['summary',`start['dateTime']`];
+  dataSrc=[];
+  calSrc=[];
+  isCal=false;
   client_id='949823352183-e1uau91hgen3uvkr9f6bhu5rldp9qiec.apps.googleusercontent.com';//sec2
   client_secret='BEGpGJ16qNhEU8F2dvVQPdIl';//sec2
   redirect_uri="http://localhost:4200";
-
+  events=[];
+  singleCal;
+  bearerToken='';
+  accessToken='';
+  resp;
+  calendars=[];
+  areCalendars=false;
   applyFilter(filterValue: string) {
     filterValue = filterValue.trim(); // Remove whitespace
     filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
-    this.dataSource.filter = filterValue;
+    this.dataSrc.filter = filterValue;
+}
+createTokenHeader(){
+  const headers= {
+    'Authorization': 'Bearer '+this.bearerToken}
+const h=new Headers(headers);
+return h;
+}
+parseDate(date:String){
+  let parsedDate = date.replace('T',' ');
+  let parsedDateFinally=parsedDate.split('+')
+  return parsedDateFinally[0];
+}
+
+getCalendars(){
+  const tokenHeader=this.createTokenHeader();
+  console.log('showing calendars...');
+  const conString='https://www.googleapis.com/calendar/v3/users/me/calendarList';
+  this.http.get(conString,{headers:tokenHeader}).subscribe(
+    (res)=>{
+      this.calendars=JSON.parse(res['_body']).items;
+      console.log('kalendarze:',this.calendars);
+      this.dataSrc=new MatTableDataSource(this.calendars);
+      if(this.dataSrc['data'].length>0){
+        this.areCalendars=true;
+      }
+
+
+    },
+    (error)=>{console.log(error);}
+  );
+}
+
+loadSingle(singleCal){
+ const tokenHeader=this.createTokenHeader();
+ const connString='https://www.googleapis.com/calendar/v3/calendars/';
+  console.log(singleCal.id);
+  this.http.get(connString+singleCal.id+'/events',{headers:tokenHeader}).subscribe(
+    (res)=>{
+      let body=res['_body'];
+      let parsedBody=JSON.parse(body);
+      this.singleCal=parsedBody.items;
+      this.calSrc=new MatTableDataSource(this.singleCal);
+      console.log(this.singleCal);
+      this.isCal=true;
+    }
+);
 }
 
 login(){
@@ -26,7 +90,45 @@ login(){
   var url='https://accounts.google.com/o/oauth2/v2/auth?redirect_uri='+this.redirect_uri+'&prompt=consent&scope='+scope+'&response_type=code&client_id='+this.client_id+'&scope'+'https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.readonly&access_type=offline'
   window.location.replace(url);
 }
+exchangeToken(){
+  let connString='https://accounts.google.com/o/oauth2/token'
+  let connBody=this.createBodyRequestForBearers();
+let headers = new Headers();
+console.log(connBody);
 
+headers.append('Content-Type', 'application/x-www-form-urlencoded');
+  this.http.post(connString,connBody,{headers:headers}).subscribe(
+    res=>{
+      console.log(res);
+      this.resp=JSON.parse(res['_body']);
+      this.bearerToken=this.resp['access_token'];
+    },
+    (error)=>{console.log(error);
+    }
+  );
+}
+
+createBodyRequestForBearers(){
+  let scope = 'https://www.googleapis.com/auth/calendar&grant_type=authorization_code'
+  let connBody='code='+encodeURI(this.accessToken);
+  connBody+='&redirect_uri='+this.redirect_uri
+  connBody+='&client_id='+this.client_id
+  connBody+='&client_secret='+this.client_secret
+  connBody+='&scope='+scope;
+  return connBody;
+}
+getAccessToken(hash:String){
+  try{
+  let a = hash.split('=')
+  let b = a[1].split('&')
+  let c = b[0].split('#')
+  console.log('token to? ',c[0]);
+  return c[0]
+  }
+  catch(ex){
+    return ""
+  }
+}
 }
 
 export interface Element {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,21 @@ import {MatTableModule} from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { NgIf } from '@angular/common';
+import {HttpModule} from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     MatButtonModule,
     MatDatepickerModule,
     BrowserAnimationsModule,
     MatSidenavModule,
+    HttpModule,
     // MatFormFieldControl,
     MatInputModule,
     MatTableModule,
